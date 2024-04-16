a7b3c41283e97ba6ed266f09247468713a0b0c23
aidenhsy <aidenhsy@hotmail.com>
2024-04-16 13:41:56 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+import myPic from '../../public/heroimage.png';
+import logo from '../../public/chatapp.svg';
+import chatimage from '../../public/chatimage.jpg';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <nav className="navbar">
+        <div className="container items-center">
+          <div className={styles.flexBetween}>
+            <div>
+              <Image
+                src={logo} // Point to the image import or URL
+                alt="Description of the image"
+              />
+            </div>
+            <div className="mobileHide">
+              <a className="navlink">Product</a>
+              <a className="navlink">Services</a>
+              <a className="navlink">Contact</a>
+              <a className="navlink">Log In</a>
+              <button className="primaryButton navButton">Try it Free</button>
+            </div>
+          </div>
         </div>
-      </div>
+      </nav>
+      {/* Hero Section  */}
+      <section className={styles.heroSection}>
+        <div className="container">
+          <div className={styles.flexBetween}>
+            <div className={styles.heroRightDiv}>
+              <h1 className={styles.whiteText}>
+                Have your
+                <br />
+                best chat
+              </h1>
+              <p className={`${styles.whiteText} mb2`}>
+                Fast, easy & unlimited chat.
+              </p>
+              <div className={styles.doubleButton}>
+                <button className={`primaryButton ${styles.whiteButton}`}>
+                  Try it Free
+                </button>
+                <button className={`primaryButton ${styles.transparentButton}`}>
+                  Try it Free
+                </button>
+              </div>
+            </div>
+            <div>
+              <Image
+                src={myPic} // Point to the image import or URL
+                alt="Description of the image"
+                layout="responsive" // Optional: Adjusts image size to fit its container
+              />
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* About Section */}
+      <section className={styles.section}>
+        <div className="container">
+          <div className={styles.columnCenterFlex}>
+            <h2>Instant Team Chats</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Donec quam felis, ultricies nec, pellentesque eu, pretium quis,
+              sem. Nulla consequat massa.
+            </p>
+            <Image
+              src={chatimage} // Point to the image import or URL
+              alt="Description of the image"
+              layout="responsive" // Optional: Adjusts image size to fit its container
+            />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
